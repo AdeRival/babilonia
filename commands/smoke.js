9fb413438b175abd30cf5acac938827afe692e23
AdeRival <40735269+AdeRival@users.noreply.github.com>
2019-03-08 21:24:36 +0700

    Update smoke.js

--- commands/smoke.js
+++ commands/smoke.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const { prefix } = require('../config.json')
+const { prefix } = require('../config.js')
 
 exports.run = async (bot, message, level) => {
     if (!message.content.startsWith(prefix)) return;
